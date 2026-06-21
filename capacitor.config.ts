dfc5d91d5a66673e0a30cca29734f709abe8f196
Enjoyer09 <132629541+Enjoyer09@users.noreply.github.com>
2026-06-21 12:48:41 +0400

    feat: integrate Capacitor mobile wrapper and add android/ios platforms

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ironwaves.loyalty',
+  appName: 'iRonWaves Loyalty',
+  webDir: 'dist'
+};
+
+export default config;
